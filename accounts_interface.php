808f30ebc7ee8bed3d0a069238380db3c66d6d70
Arina Potekhina <a.potekhina@g.nsu.ru>
2021-07-01 21:23:46 +0700

    add function showAccountsList

--- accounts_interface.php
+++ accounts_interface.php
@@ -1,10 +1,16 @@
 <?php
 
+require_once 'database.php';
+
 class AccountsInterface { 
+	private $db;
+
+	function __construct() {
+	  
+      $this->db = new DataBase();
+    }
 
 	public function createAccount($first_name, $last_name, $email, $company_name, $position, $phone1, $phone2, $phone3){
-		require_once 'database.php';
-		$bd = new DataBase();
 		$bd->addUserInf($first_name, $last_name, $email, $company_name, $position, $phone1, $phone2, $phone3);
 		unset($bd);
 	}
@@ -13,7 +19,18 @@ class AccountsInterface {
 	
 //	public function updateAccount();
 	
-	//public function getAccountsList();
+	public function showAccountsList(){
+		$emails = $this->db->getEmailsList();
+		//pagination
+		echo '<table border="1">';
+		foreach($emails as $id => $email) {
+			echo '<tr>';
+			echo '<td><a href="update_account_page.php?id='.$id.'">'.$email.'</href></td>';
+			echo '</tr>';
+		}
+		echo '</tr>';
+		echo '</table>';
+	}
 }
 
 ?>
