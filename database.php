3bf7d92289046dfdf5579a0d2d18b770ed9021da
Arina Potekhina <a.potekhina@g.nsu.ru>
2021-07-04 22:13:02 +0700

    change change in function updateUserInf for updating phones

--- database.php
+++ database.php
@@ -44,32 +44,31 @@ class DataBase{
 		$statement2->bind_param("is", $user_id, $phone);
 	
 
-		if(isset($phone1)) 
+		if(isset($phone1) && !empty($phone1)) 
 		{
 			$phone = $phone1;
-			echo $phone;
 			$statement2->execute();
 		}
 		
-		if(isset($phone2))
+		if(isset($phone2) && !empty($phone2))
 		{
 			$phone = $phone2;
 			$statement2->execute();
 		}
 		
-		if(isset($phone3))
+		if(isset($phone3) && !empty($phone3))
 		{
 			$phone = $phone3;
 			$statement2->execute();
 		}
 	}
 	
-	public function getEmailsList(){
+	public function getEmailsList($start_with, $rows_number_on_page){
 		$emailsList;
-		$query = 'SELECT id, email FROM users';
+		$query = 'SELECT id, email FROM users LIMIT '.$start_with.', '.$rows_number_on_page;
 		$result = mysqli_query($this->connection, $query);
 		if(!$result)
-			die('upadting error: '. mysql_error());
+			die(mysql_error());
 		while($row = mysqli_fetch_assoc($result)) {
 			$emailsList[$row['id']] = $row['email'];
 		}
@@ -88,26 +87,24 @@ class DataBase{
 		$query = 'SELECT * FROM phone_numbers WHERE user_id='.$id;
 		$result = mysqli_query($this->connection, $query);
 		if(!$result)
-			die('upadting error: '. mysql_error());
+			die(mysql_error());
 		$counter = 1;
 		while($row = mysqli_fetch_assoc($result)) {
 			$userInf['phone'.$counter] = $row['phone_number'];
 			$counter++;
 		}
-		
 		return $userInf;
 	}
 	
 	public function updateUserInf($userInf){
-		$query = 'UPDATE users SET first_name="'.$userInf['first_name'].'", last_name="'.$userInf['last_name'].'", email="'.$userInf['email'].'"'.;
+		$query = 'UPDATE users SET first_name="'.$userInf['first_name'].'", last_name="'.$userInf['last_name'].'", email="'.$userInf['email'].'"';
 		$company_name = $userInf['company_name'];
 		$position = $userInf['position'];
 		if(!empty($company_name))
-			$query.', "'.$company_name.'"';
+			$query = $query.', company_name="'.$company_name.'"';
 		if(!empty($position))
-			$query.', "'.$position.'"';
-		$query.' WHERE id='.$userInf['id'];
-		
+			$query = $query.', position="'.$position.'"';
+		$query = $query.' WHERE id='.$userInf['id'];
 		$result = mysqli_query($this->connection, $query);
 		if(!$result)
 			die('upadting error: '. mysql_error());
@@ -116,29 +113,44 @@ class DataBase{
 		$phone2 = $userInf['phone2'];
 		$phone3 = $userInf['phone3'];
 		$user_id = $userInf['id'];
-
-		if(!empty($phone1))
-			$this->updatePhone($phone1, $user_id);
-		if(!empty($phone2))
-			$this->updatePhone($phone2, $user_id);
-		if(!empty($phone3))
-			$this->updatePhone($phone3, $user_id);
 		
+		$this->deleteUserPhones($user_id);
+		if(!empty($phone1))
+			$this->addPhone($user_id, $phone1);
+		if(!empty($phone2))
+			$this->addPhone($user_id, $phone2);
+		if(!empty($phone3))
+			$this->addPhone($user_id, $phone3);
 	}
 	
-	private function updatePhone($phone, $user_id){
-		$query = 'UPDATE phone_numbers SET phone_number="'.$phone.'" WHERE user_id='.$id;
-		$result = mysqli_query($this->connection, $query);
-		if(!$result)
-			die('upadting error: '. mysql_error());
+	private function addPhone($user_id, $phone){
+		//echo $user_id."  ".$phone;
+		$statement = $this->connection->prepare('INSERT INTO phone_numbers(user_id, phone_number) VALUES(?, ?)'); 
+		$statement->bind_param("is", $user_id, $phone);
+		$statement->execute();
 	}
 	
 	public function deleteUserInf($id){
 		$query = 'DELETE FROM users WHERE id='.$id;
 		$result = mysqli_query($this->connection, $query);
 		if(!$result)
-			die('upadting error: '. mysql_error());
+			die('deleting error: '. mysql_error());
 		//cascade deleting for phones 
+	}
+	
+	private function deleteUserPhones($user_id){
+		$query = 'DELETE FROM phone_numbers WHERE user_id='.$user_id;
+		//echo $query;
+		$result = mysqli_query($this->connection, $query);
+		if(!$result)
+			die('deleting error: '. mysql_error());
+	}
+	
+	public function getAllUsersNumber(){
+		$result = mysqli_query($this->connection, 'SELECT COUNT(*) FROM users');
+		$row = mysqli_fetch_row($result);
+		$all_rows_number = $row[0]; 
+		return $all_rows_number;
 	}
 }
 
