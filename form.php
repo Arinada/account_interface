c0b88de6672207c58cbbec2dc19736a96de899e1
Arina Potekhina <a.potekhina@g.nsu.ru>
2021-07-05 13:00:10 +0700

    delete mask for phone number

--- form.php
+++ form.php
@@ -4,14 +4,14 @@ function renderForm($userInf) {
 	echo '<!DOCTYPE html><head><meta charset="utf-8"><title>Главная страница</title></head>';
 	echo '<body> <form action="controller.php">';  
 	echo '<input type="hidden"  name="id" value="'.$userInf['id'].'">';
-	echo '<p>Имя: <input value="'.$userInf['first_name'].'" maxlength="25" size="40" name="first_name"></p>';
-	echo '<p>Фамилия: <input value="'.$userInf['last_name'].'" maxlength="25" size="40" name="last_name"></p>';
-	echo '<p>Электронная почта: <input value="'.$userInf['email'].'" type="email" maxlength="25" size="40" name="email"></p>';
-	echo '<p>Название компании: <input value="'.$userInf['company_name'].'" maxlength="25" size="60" name="company_name"></p>';
-	echo '<p>Должность: <input value="'.$userInf['position'].'" maxlength="25" size="60" name="position"></p>';
-	echo '<p>Номер телефона в формате 7-xxx-xxx-xx-xx: <input value="'.$userInf['phone1'].'" type="tel" pattern="7-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}" maxlength="25" size="20" name="phone1"></p>';
-	echo '<p>Номер телефона: <input value="'.$userInf['phone2'].'" type="tel" maxlength="25" size="20" name="phone2"></p>';
-	echo '<p>Номер телефона: <input value="'.$userInf['phone3'].'" type="tel" maxlength="25" size="20" name="phone3"></p>';
+	echo '<p>Имя:</p> <input value="'.$userInf['first_name'].'" maxlength="25" name="first_name">';
+	echo '<p>Фамилия:</p> <input value="'.$userInf['last_name'].'" maxlength="25" name="last_name">';
+	echo '<p>Электронная почта:</p> <input value="'.$userInf['email'].'" type="email" maxlength="25" name="email">';
+	echo '<p>Название компании:</p> <input value="'.$userInf['company_name'].'" maxlength="25" name="company_name">';
+	echo '<p>Должность:</p> <input value="'.$userInf['position'].'" maxlength="25" name="position">';
+	echo '<p>Номер телефона: </p><input value="'.$userInf['phone1'].'" type="tel" " maxlength="25" name="phone1">';
+	echo '<p>Номер телефона: </p><input value="'.$userInf['phone2'].'" type="tel" maxlength="25" name="phone2">';
+	echo '<p>Номер телефона: </p><input value="'.$userInf['phone3'].'" type="tel" maxlength="25" name="phone3">';
 }
 
 ?>
