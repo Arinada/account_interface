178e95635a281dec7226c029188b7502f9a096c0
Arina Potekhina <a.potekhina@g.nsu.ru>
2021-07-05 12:50:51 +0700

    move routing to main page in function

--- accounts_interface.php
+++ accounts_interface.php
@@ -1,38 +1,38 @@
 <?php
 
 require_once 'database.php';
+require_once 'accounts_list.php';
 
 class AccountsInterface { 
 	private $db;
 
 	function __construct() {
-	  
       $this->db = new DataBase();
     }
 
 	public function createAccount($first_name, $last_name, $email, $company_name, $position, $phone1, $phone2, $phone3){
 		$this->db->addUserInf($first_name, $last_name, $email, $company_name, $position, $phone1, $phone2, $phone3);
-		header('Location: main_page.php');
-		exit;
-		unset($this->db);
+		$this->turnToMainPage();
 	}
 
 	public function deleteAccount($id){
 		$this->db->deleteUserInf($id);
-		header('Location: main_page.php');
-		exit;
-		unset($this->db);
+		$this->turnToMainPage();
 	}
 	
 	public function updateAccount($userInf){
 		$this->db->updateUserInf($userInf);
+		$this->turnToMainPage();
+	}
+	
+	public function showAccountsList($page_number){	
+		formAccountsList($page_number);
+	}
+	
+	private function turnToMainPage(){
 		header('Location: main_page.php');
 		exit;
 		unset($this->db);
-	}
-	
-	public function showAccountsList(){	
-		require_once 'accounts_list.php';
 	}
 	
 }
